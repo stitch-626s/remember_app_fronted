80eeea37316e3beeb858469bd8ed765197306d01
stitch-626s <3416013400@qq.com>
2026-03-14 21:05:55 +0800

    添加frontend_vue

---
+++ frontend_vue/remember_app/src/views/PageHome.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="home-layout">
+    <el-container>
+      <el-aside class="sidebar-container">
+            <el-menu
+                default-active="2"
+                class="el-menu-vertical"
+            >
+                <!-- 带有下拉列表的组件 -->
+                <el-sub-menu index="1">
+                <template #title>
+                    <el-icon><home-filled /></el-icon>
+                    <span>首页</span>
+                </template>
+                </el-sub-menu>
+
+                <!-- 无下拉列表 -->
+                <el-menu-item index="2">
+                <el-icon><icon-menu /></el-icon>
+                <template #title>背题</template>
+                </el-menu-item>
+
+                <el-menu-item index="3">
+                <el-icon><document /></el-icon>
+                <template #title>题库</template>
+                </el-menu-item>
+
+                <el-menu-item index="4">
+                <el-icon><setting /></el-icon>
+                <template #title>个人主页</template>
+                </el-menu-item>
+            </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+
+      <el-aside width="200px">Aside</el-aside>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+import {
+  Document,
+  HomeFilled,
+  Menu as IconMenu,
+  Setting,
+} from '@element-plus/icons-vue'
+</script>
+
+<style lang="css" scoped>
+    @import '../assets/styles/home.css'
+</style>
---
+++ frontend_vue/remember_app/src/views/PageRegister.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="register-box">
+    <h3 class="title">注册</h3>
+
+    <div class="input-area">
+      <p id="email">电子邮件</p>
+      <el-input v-model="input.username" class="input-email" placeholder="输入邮箱账号" />
+      <p id="password">密码</p>
+      <el-input v-model="input.password" class="input-password" placeholder="输入密码" />
+    </div>
+
+    <div>
+      <div class="primary-btn">
+        <el-button type="primary" class="register-btn">注册</el-button>
+      </div>
+    </div>
+      
+  </div>
+</template>
+
+<script setup >
+  import { ref } from 'vue'
+
+  const input = ref({
+    username: "",
+    password: ""
+  })
+
+</script>
+
+<style>
+    @import '../assets/styles/register.css'
+</style>
