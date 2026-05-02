f6b57bc3d7797092cd0db744e4377f07ade4a09f
stitch-626s <3416013400@qq.com>
2026-05-02 12:39:47 +0800

    修改引用错误

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import router from "../router/route";
 import { ElMessage } from "element-plus";
 
 const request = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_BASE_URL || "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
   timeout: 10000,
 });
 
